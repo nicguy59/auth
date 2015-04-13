df43dde64dd1a4eb6f1ebe8582561c5fb419be47
nicguy59 <mowen@northislandcollege.ca>
2015-04-13 14:20:42 -0700

    Update user_controller.rb

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -29,7 +29,7 @@ class UserController < ApplicationController
 private
 
 def user_params
-  params.require(:user).permit(:name, :email, :passwork_confirmation)
+  params.require(:user).permit(:name, :email, :password_confirmation)
 end
 
 def lookup_user
